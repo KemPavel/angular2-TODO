c893174ef34c2c4fe39938d62e31b9393e2ef057
KemPavel <pkazakov18@gmail.com>
2017-09-17 22:41:20 +0400

    add blank files for future components

---
+++ src/app/components/courses/course/index.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+export class Course {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+  duration: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
+import './assets/css/reset.css';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
